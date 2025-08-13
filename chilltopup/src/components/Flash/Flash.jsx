b703885b17a5da2bd79d38cd9c65adc5371bb348
hazeco <thunderwolf2209@gmail.com>
2025-08-14 02:49:25 +0700

    Update Flash.jsx

--- chilltopup/src/components/Flash/Flash.jsx
+++ chilltopup/src/components/Flash/Flash.jsx
@@ -10,13 +10,70 @@ function Flash() {
         <img src="" alt="" />
         <img src="" alt="" />
         {/* container */}
-        <div>
+        <div className='w-full p-12 mx-auto '>
           {/* header */}
-          <div className="">
+          <div className="text-center pb-4">
             <h1 className='text-[#000] text-[70px] leading-[72px] font-heavent font-bold'>FLASH SALE</h1>
             <div className='flex justify-center'>
               {/* underline */}
               <div className='w-24 border-8 border-primary'></div>
+            </div>
+          </div>
+          <div>
+            {/* row */}
+            <div>
+              {/* col */}
+              <div>
+                {/* card */}
+                <div>
+                  {/* thumnail */}
+                  <div>
+                    <div>
+                      {/* responsive sizer */}
+                      <div></div>
+                      {/* image */}
+                      <div>
+                        <img src="https://gameon.ais.co.th/api/cms-data-service/product/461d6a18-8275-4d53-bb97-7db604144b4f-ign-GameOn-thumbnail_Valorant-5.jpg" alt="" />
+                      </div>
+                      {/* responsive_contetnt */}
+                      <div></div>
+                    </div>
+                  </div>
+                  {/* overlay */}
+                  <div></div>
+                  {/* overlay_scrim */}
+                  <div></div>
+                  {/* overlay_content */}
+                  <div>
+                    <span>SALE NOW</span>
+                    {/* chip */}
+                    <span>
+                      {/* chip_content */}
+                      <span>
+                        <p>93</p>
+                      </span>
+                    </span>
+                    -:-
+                    {/* chip */}
+                    <span>
+                      {/* chip_content */}
+                      <span>
+                        <p>54</p>
+                      </span>
+                    </span> 
+                    -:-
+                    {/* chip */}
+                    <span>
+                      {/* chip_content */}
+                      <span>
+                        <p>23</p>
+                      </span>
+                    </span>
+                  </div>
+                  {/* card_promotion_title */}
+                  <h3>VALORANT</h3>
+                </div>
+              </div>
             </div>
           </div>
           
