3b63650a2e1f86a4ad7cd267357dd3fe233cd22b
KumPong <ananyodchaichana@gmail.com>
2025-08-14 17:54:17 +0700

    Update App.jsx

--- chilltopup/src/App.jsx
+++ chilltopup/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
 
 import Flash from './components/Flash/Flash.jsx'
+import Hero from './components/Hero/Hero.jsx'
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Hero/>
       <Flash />
     </>
   )
