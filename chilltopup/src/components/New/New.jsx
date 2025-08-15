2f1bb7d19eb38534abb8f8bdcbd2b1ecf1f5c14c
hazeco <thunderwolf2209@gmail.com>
2025-08-15 17:08:44 +0700

    new

--- chilltopup/src/components/New/New.jsx
+++ chilltopup/src/components/New/New.jsx
@@ -18,7 +18,7 @@ function New() {
                                     {/* tablist */}
                                     <div className='h-[84px] bg-transparent text-primary caret-primary
                                     flex-initial relative w-full ease-[cubic-bezier(0.25,0.8,0.5,1)] 
-                                    duration-300 flex rounded-[inherit] '>
+                                    duration-300 flex rounded-[inherit]'>
                                         {/* slide-group-prev */}
                                         <div className='cursor-pointer hidden pointer-events-none
                                         items-center  flex-[0_1_52px] justify-center min-w-[52px]'></div>
@@ -26,42 +26,55 @@ function New() {
                                         <div className='cursor-auto [contain:content] flex flex-auto
                                         overflow-hidden '>
                                             {/* slide-group-content */}
-                                            <div className='flex-none relative ease-[cubic-bezier(0.25,0.8,0.5,1)]
-                                            whitespace-nowrap'>
+                                            <div className='flex-none flex relative ease-[cubic-bezier(0.25,0.8,0.5,1)]
+                                            whitespace-nowrap duration-300 cursor-auto'>
                                                 {/* tabs-slider-warper */}
                                                 <div className='h-0.5 left-0 w-[186px] bottom-0 m-0 absolute
                                                 ease-[cubic-bezier(0.25,0.8,0.5,1)] z-[1]'>
                                                     {/* tabs-slider */}
-                                                    <div className='bg-primary border-primary bg-transparent
-                                                    border-transparent w-[186px] h-[84px] m-[-74px] rounded-[5px]'></div>
+                                                    <div className='bg-transparent border-transparent w-[186px] h-[84px] mt-[-74px] rounded-[5px] z-0'></div>
                                                 </div>
                                                 {/* tabs-active */}
-                                                <div>
+                                                <div className='mr-[44px] whitespace-normal m-[unset] p-[unset] text-[inherit] text-primary items-center 
+                                                cursor-pointer flex flex-initial text-sm font-medium justify-center tracking[.0892857143em] leading-[normal] 
+                                                min-w-[90px] max-w-[360px] outline-none relative text-center no-underline normal-case transition-none
+                                                select-none z-0'>
                                                     {/* list-item */}
-                                                    <div>
+                                                    <div className='text-white bg-primary w-[186px] h-[84px] rounded-md border-[1px] border-[#606060] 
+                                                    items-center flex flex-[1_1_100%] tracking-[normal] min-h-12 outline-none outline-offset-0  outline-0 
+                                                    px-[0px] py-[16px] relative no-underline'>
                                                         {/* list-item-icon */}
-                                                        <div>
-                                                            <img className='w-[37.23px] h-[52px]' src="https://gameon.ais.co.th/assets/_nuxt/img/review_ic_on.92c94e2.png" alt="gameon" />
+                                                        <div className='mr-0 self-center ml-[24px] inline-flex min-w-[24px] mx-[0px] my-[16px]'>
+                                                            <img className='w-auto h-[52px] max-w-[60px] max-h-[52px] p-0 z-10 border-none'
+                                                            src="https://gameon.ais.co.th/assets/_nuxt/img/review_ic_on.92c94e2.png" alt="gameon" />
                                                         </div>
                                                         {/* list-item-content */}
-                                                        <div>
+                                                        <div className='items-center self-center flex flex-wrap flex-[1_1] overflow-hidden py-[12px] px-[0px]'>
                                                             {/* list-item-title */}
-                                                            <div> Game Reviews </div>
+                                                            <div className='text-[#000] pl-[12px] text-left text-[22px] leading-[1.2] self-center flex-[1_1_100%]
+                                                            overflow-hidden'> Game Reviews </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                                 {/* tab */}
-                                                <div>
+                                                <div className='text-white mr-[44px] whitespace-normal m-[unset] p-[unset] items-center 
+                                                cursor-pointer flex flex-initial text-sm font-medium justify-center tracking[.0892857143em] leading-[normal] 
+                                                min-w-[90px] max-w-[360px] outline-none relative text-center no-underline normal-case transition-none
+                                                select-none z-0'>
                                                     {/* list-item */}
-                                                    <div>
+                                                    <div className='text-white bg-transparent w-[186px] h-[84px] rounded-md border-[1px] border-[#606060] 
+                                                    items-center flex flex-[1_1_100%] tracking-[normal] min-h-12 outline-none outline-offset-0  outline-0 
+                                                    px-[0px] py-[16px] relative no-underline'>
                                                         {/* list-item-icon */}
-                                                        <div>
-                                                            <img className='w-[42px] h-[52px]' src="https://gameon.ais.co.th/assets/_nuxt/img/game_guide_ic.bebe220.png" alt="gameon" />
+                                                        <div className='mr-0 self-center ml-[24px] inline-flex min-w-[24px] mx-[0px] my-[16px]'>
+                                                            <img className='w-auto h-[52px] max-w-[60px] max-h-[52px] p-0 z-10 border-none'
+                                                            src="https://gameon.ais.co.th/assets/_nuxt/img/game_guide_ic.bebe220.png" alt="gameon" />
                                                         </div>
                                                         {/* list-item-content */}
-                                                        <div>
+                                                        <div className='items-center self-center flex flex-wrap flex-[1_1] overflow-hidden py-[12px] px-[0px]'>
                                                             {/* list-item-title */}
-                                                            <div> Game Guides </div>
+                                                            <div className='pl-[12px] text-left text-[22px] leading-[1.2] self-center flex-[1_1_100%]
+                                                            overflow-hidden'> Game Guides </div>
                                                         </div>
                                                     </div>
                                                 </div>
